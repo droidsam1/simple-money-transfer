3a11fcd5d1f0022bf4ec202c4cc271d4493f35dc
staboas <staboas@leadtech.com>
2024-07-09 10:48:24 +0200

    refactor: push transactional responsibility to repository

--- src/main/java/com/example/simple/bank/repository/AccountRepository.java
+++ src/main/java/com/example/simple/bank/repository/AccountRepository.java
@@ -2,6 +2,7 @@ package com.example.simple.bank.repository;
 
 import com.example.simple.bank.Account;
 import com.example.simple.bank.AccountId;
+import com.example.simple.bank.Money;
 import java.util.Optional;
 
 public interface AccountRepository {
@@ -9,4 +10,6 @@ public interface AccountRepository {
     void register(Account account);
 
     Optional<Account> get(AccountId accountId);
+
+    void transfer(AccountId from, AccountId to, Money amount);
 }
--- src/main/java/com/example/simple/bank/repository/pessimistic/GlobalLockManagerAccountRepository.java
+++ src/main/java/com/example/simple/bank/repository/pessimistic/GlobalLockManagerAccountRepository.java
@@ -1,10 +1,13 @@
-package com.example.simple.bank.repository;
+package com.example.simple.bank.repository.pessimistic;
 
 import com.example.simple.bank.Account;
 import com.example.simple.bank.AccountId;
+import com.example.simple.bank.Money;
 import com.example.simple.bank.distributed.GlobalLockManager;
 import com.example.simple.bank.distributed.LockManager;
 import com.example.simple.bank.exceptions.AccountAlreadyRegisteredException;
+import com.example.simple.bank.exceptions.AccountNotFoundException;
+import com.example.simple.bank.repository.AccountRepository;
 import java.util.Map;
 import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
@@ -32,4 +35,16 @@ public class GlobalLockManagerAccountRepository implements AccountRepository {
     @Override public Optional<Account> get(AccountId accountId) {
         return Optional.ofNullable(this.accountRepository.get(accountId));
     }
+
+    @Override public void transfer(AccountId from, AccountId to, Money amount) {
+        var fromAccount = this.get(from).orElseThrow(AccountNotFoundException::new);
+        var toAccount = this.get(to).orElseThrow(AccountNotFoundException::new);
+
+        lockManager.performAtomic(from, to, () -> {
+            fromAccount.withdraw(amount);
+            toAccount.deposit(amount);
+        });
+    }
+
+
 }
