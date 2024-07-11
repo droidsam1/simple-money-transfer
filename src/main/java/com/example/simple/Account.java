e5ca44d38ee93e93c149491fb13e0255eb8974bf
staboas <staboas@leadtech.com>
2024-07-11 16:56:43 +0200

    timed attempt to refactor by an optimistic approach

--- src/main/java/com/example/simple/Account.java
+++ src/main/java/com/example/simple/Account.java
@@ -4,19 +4,20 @@ import com.example.simple.exceptions.InsufficientFundsException;
 import com.example.simple.exceptions.MisMatchCurrencyException;
 import java.math.BigDecimal;
 import java.util.Objects;
+import java.util.concurrent.atomic.AtomicReference;
 
 public class Account {
 
     private final String id;
-    private Money balance;
+    private final AtomicReference<Money> balance;
 
     public Account(String id, Money balance) {
         this.id = Objects.requireNonNull(id);
-        this.balance = Objects.requireNonNull(balance);
+        this.balance = new AtomicReference<>(Objects.requireNonNull(balance));
     }
 
     public Money balance() {
-        return this.balance;
+        return this.balance.get();
     }
 
     public void transfer(Account beneficiary, Money balanceToTransfer) {
@@ -25,6 +26,37 @@ public class Account {
         validateEnoughFunds(balanceToTransfer);
 
         //atomic operation
+        optimisticTransferStrategy(beneficiary, balanceToTransfer);
+    }
+
+    private void optimisticTransferStrategy(Account beneficiary, Money balanceToTransfer) {
+        boolean withdraw;
+        boolean deposit;
+
+        while (true) {
+            var expectedSenderBalance = this.balance.get();
+            var expectedBeneficiaryBalance = beneficiary.balance.get();
+
+            withdraw = this.balance.compareAndSet(
+                    expectedSenderBalance,
+                    expectedSenderBalance.subtract(balanceToTransfer)
+            );
+            if (withdraw) {
+                deposit = beneficiary.balance.compareAndSet(
+                        expectedBeneficiaryBalance,
+                        expectedBeneficiaryBalance.add(balanceToTransfer)
+                );
+                if (deposit) {
+                    return;
+                } else {
+                    //rollback: naive implementation
+                    this.deposit(balanceToTransfer);
+                }
+            }
+        }
+    }
+
+    private void pessimisticTransferStrategy(Account beneficiary, Money balanceToTransfer) {
         if (this.id.compareToIgnoreCase(beneficiary.id) > 0) {
             synchronized (this) {
                 synchronized (beneficiary) {
@@ -43,11 +75,11 @@ public class Account {
     }
 
     private void deposit(Money balanceToTransfer) {
-        this.balance = this.balance.add(balanceToTransfer);
+        this.balance.updateAndGet(b -> b.add(balanceToTransfer));
     }
 
     private void withdraw(Money balanceToTransfer) {
-        this.balance = this.balance.subtract(balanceToTransfer);
+        this.balance.updateAndGet(b -> b.subtract(balanceToTransfer));
     }
 
     private void validateEnoughFunds(Money balanceToTransfer) {
@@ -63,7 +95,7 @@ public class Account {
     }
 
     private void validateCurrency(Money balanceToTransfer) {
-        if (!this.balance.currency().equals(balanceToTransfer.currency())) {
+        if (!this.balance.get().currency().equals(balanceToTransfer.currency())) {
             throw new MisMatchCurrencyException();
         }
     }
