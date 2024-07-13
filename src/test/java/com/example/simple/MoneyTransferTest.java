8990de103f91af8ddd4ba74aee5e933bbbe01c6f
staboas <staboas@leadtech.com>
2024-07-13 09:31:48 +0200

    add a concurrent test, 3 min

--- src/test/java/com/example/simple/MoneyTransferTest.java
+++ src/test/java/com/example/simple/MoneyTransferTest.java
@@ -3,6 +3,9 @@ package com.example.simple;
 import com.example.simple.exceptions.InsufficientFundsException;
 import com.example.simple.exceptions.MismatchCurrencyException;
 import com.example.simple.exceptions.NegativeTransferAmountException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -49,8 +52,9 @@ class MoneyTransferTest {
 
         Assertions.assertThrows(MismatchCurrencyException.class, () -> john.transfer(jane, dollars(1)));
     }
+
     @Test
-    void shouldTransferMoneyFromDifferentAccount(){
+    void shouldTransferMoneyFromDifferentAccount() {
         Account john = new Account("John", dollars(10));
         Account jane = new Account("Jane", dollars(0));
 
@@ -59,8 +63,26 @@ class MoneyTransferTest {
 
         Assertions.assertEquals(dollars(10), john.balance());
         Assertions.assertEquals(dollars(0), jane.balance());
+    }
 
+    @Test
+    void shouldTransferMoneyConcurrently() {
+        Account john = new Account("John", dollars(1000));
+        Account jane = new Account("Jane", dollars(1000));
+        Account jack = new Account("Jack", dollars(1000));
 
+        List<CompletableFuture<Void>> transferTasks = new ArrayList<>();
+        for (int i = 0; i < 1000; i++) {
+            transferTasks.add(CompletableFuture.runAsync(() -> john.transfer(jane, dollars(1))));
+            transferTasks.add(CompletableFuture.runAsync(() -> jane.transfer(jack, dollars(1))));
+            transferTasks.add(CompletableFuture.runAsync(() -> jack.transfer(john, dollars(1))));
+
+        }
+        transferTasks.forEach(CompletableFuture::join);
+
+        Assertions.assertEquals(dollars(1000), john.balance());
+        Assertions.assertEquals(dollars(1000), jane.balance());
+        Assertions.assertEquals(dollars(1000), jack.balance());
     }
 
 
