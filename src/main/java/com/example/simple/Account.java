9579522af3fb091ec56190f0b6efe8e3429ab5e0
staboas <staboas@leadtech.com>
2024-07-14 15:32:02 +0200

    timed attempt: +4 min for optimistic transfer approach

--- src/main/java/com/example/simple/Account.java
+++ src/main/java/com/example/simple/Account.java
@@ -1,23 +1,45 @@
 package com.example.simple;
 
 import com.example.simple.exceptions.InsufficientFundsException;
+import java.util.concurrent.atomic.AtomicReference;
 
 public class Account {
 
     private final AccountId id;
-    private Money balance;
+    private final AtomicReference<Money> balance;
 
     public Account(String id, Money initialBalance) {
         this.id = new AccountId(id);
-        this.balance = initialBalance;
+        this.balance = new AtomicReference<>(initialBalance);
     }
 
     public Money balance() {
-        return balance;
+        return balance.get();
     }
 
     public void transfer(Account recipient, Money transferFunds) {
+        optimisticTransferApproach(recipient, transferFunds);
+    }
 
+    private void optimisticTransferApproach(Account recipient, Money transferFunds) {
+        while (true) {
+            Money expectedSenderBalance = balance.get();
+            Money expectedRecipientBalance = recipient.balance();
+            if (this.balance.compareAndSet(expectedSenderBalance, expectedSenderBalance.subtract(transferFunds))) {
+                if (recipient.balance.compareAndSet(
+                        expectedRecipientBalance,
+                        expectedRecipientBalance.add(transferFunds)
+                )) {
+                    break;
+                } else {
+                    //rollback: naive implementation
+                    this.deposit(transferFunds);
+                }
+            }
+        }
+    }
+
+    private void pessimisticTransferApproach(Account recipient, Money transferFunds) {
         if (this.id.compareTo(recipient.id) > 0) {
             synchronized (this) {
                 synchronized (recipient) {
@@ -38,16 +60,15 @@ public class Account {
 
     private void withdraw(Money transferFunds) {
         validateEnoughFunds(transferFunds);
-        this.balance = this.balance.subtract(transferFunds);
+        this.balance.updateAndGet(b -> b.subtract(transferFunds));
     }
 
     private void deposit(Money transferFunds) {
-        this.balance = this.balance.add(transferFunds);
+        this.balance.updateAndGet(b -> b.add(transferFunds));
     }
 
     private void validateEnoughFunds(Money transferFunds) {
-
-        if (balance.amount().compareTo(transferFunds.amount()) < 0) {
+        if (balance.get().amount().compareTo(transferFunds.amount()) < 0) {
             throw new InsufficientFundsException();
         }
     }
