1e6bfe46980abc6c6096da8c15576115ec780d0d
staboas <staboas@leadtech.com>
2024-07-17 19:49:43 +0200

    start over

--- src/main/java/com/example/simple/exceptions/MisMatchCurrencyException.java
+++
@@ -1,9 +0,0 @@
-package com.example.simple.exceptions;
-
-public class MisMatchCurrencyException extends IllegalArgumentException {
-
-
-    public MisMatchCurrencyException() {
-        super("MisMatch currency");
-    }
-}
