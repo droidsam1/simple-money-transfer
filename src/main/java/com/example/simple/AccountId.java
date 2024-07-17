599cf11bc8d1360a62b42c515c9259d9542b0c1e
staboas <staboas@leadtech.com>
2024-07-17 10:53:10 +0200

    timed attempt: +4 min for a concurrent pessimistic transfer approach

--- src/main/java/com/example/simple/AccountId.java
+++ src/main/java/com/example/simple/AccountId.java
@@ -1,5 +1,8 @@
 package com.example.simple;
 
-public record AccountId(String value) {
+public record AccountId(String value) implements Comparable<AccountId> {
 
+    @Override public int compareTo(AccountId o) {
+        return this.value.compareToIgnoreCase(o.value);
+    }
 }
--- src/test/java/com/example/simple/MoneyTransferTest.java
+++ src/test/java/com/example/simple/MoneyTransferTest.java
@@ -1,6 +1,10 @@
 package com.example.simple;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 
 class MoneyTransferTest {
@@ -23,6 +27,23 @@ class MoneyTransferTest {
 
         Assertions.assertEquals(eur(50), john.balance());
         Assertions.assertEquals(eur(50), jane.balance());
+    }
+
+
+    @RepeatedTest(100)
+    void shouldTransferMoneyBetweenAccountsConcurrently() {
+        Account john = new Account("John", eur(100));
+        Account jane = new Account("Jane", eur(100));
+
+        List<CompletableFuture<Void>> transferTasks = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            transferTasks.add(CompletableFuture.runAsync(() -> john.transfer(jane, eur(1))));
+            transferTasks.add(CompletableFuture.runAsync(() -> jane.transfer(john, eur(1))));
+        }
+        transferTasks.forEach(CompletableFuture::join);
+
+        Assertions.assertEquals(eur(100), john.balance());
+        Assertions.assertEquals(eur(100), jane.balance());
     }
 
     private static Money eur(int amount) {
