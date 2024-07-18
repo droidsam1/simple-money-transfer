3ee905c2d0b4aeb39c18d6e39f02b83292f8b46c
staboas <staboas@leadtech.com>
2024-07-18 07:31:40 +0200

    start over

--- src/main/java/com/example/simple/exceptions/NegativeAmountTransferException.java
+++
@@ -1,8 +0,0 @@
-package com.example.simple.exceptions;
-
-public class NegativeAmountTransferException extends IllegalArgumentException {
-
-    public NegativeAmountTransferException() {
-        super("Negative amount transfer are not allowed");
-    }
-}
