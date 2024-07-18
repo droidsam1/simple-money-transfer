f2362176067e84a8de8891371e8d6d6c0d7f50c2
staboas <staboas@leadtech.com>
2024-07-18 07:56:24 +0200

    timed attempt: +6 min to implement optimistic transfer approach

--- src/main/java/com/example/simple/Account.java
+++ src/main/java/com/example/simple/Account.java
@@ -3,53 +3,56 @@ package com.example.simple;
 import com.example.simple.exceptions.InsufficientFundsException;
 import com.example.simple.exceptions.NegativeTransferAttemptException;
 import java.math.BigDecimal;
+import java.util.concurrent.atomic.AtomicReference;
 
 public class Account {
 
     private final AccountId id;
-    private Money balance;
+    private final AtomicReference<Money> balance;
 
     public Account(String id, Money balance) {
         this.id = new AccountId(id);
-        this.balance = balance;
+        this.balance = new AtomicReference<>(balance);
     }
 
     public Money balance() {
-        return balance;
+        return balance.get();
     }
 
     public void transfer(Account recipient, Money funds) {
         validateFundsArePositive(funds);
         //this should be atomic
 
-        if (this.id.compareTo(recipient.id) > 0) {
-            synchronized (this) {
-                synchronized (recipient) {
-                    this.withdraw(funds);
-                    recipient.deposit(funds);
-                }
-            }
-        } else {
-            synchronized (recipient) {
-                synchronized (this) {
-                    this.withdraw(funds);
-                    recipient.deposit(funds);
+        while (true) {
+            Money senderExpectedBalance = this.balance.get();
+            Money receiverExpectedBalance = recipient.balance();
+            validateEnoughFunds(funds, senderExpectedBalance);
+
+            if (this.balance.compareAndSet(senderExpectedBalance, senderExpectedBalance.subtract(funds))) {
+                if (recipient.balance.compareAndSet(receiverExpectedBalance, receiverExpectedBalance.add(funds))) {
+                    //success
+                    break;
+                } else {
+                    //rollback withdraw
+                    this.deposit(funds);
                 }
             }
         }
     }
 
     private void deposit(Money funds) {
-        this.balance = this.balance().add(funds);
+        this.balance.updateAndGet(b -> b.add(funds));
     }
 
     private void withdraw(Money funds) {
-        validateEnoughFunds(funds);
-        this.balance = this.balance().subtract(funds);
+        this.balance.updateAndGet(b -> {
+            validateEnoughFunds(funds, b);
+            return b.subtract(funds);
+        });
     }
 
-    private void validateEnoughFunds(Money funds) {
-        if (this.balance().amount().compareTo(funds.amount()) < 0) {
+    private static void validateEnoughFunds(Money funds, Money b) {
+        if (b.amount().compareTo(funds.amount()) < 0) {
             throw new InsufficientFundsException();
         }
     }
